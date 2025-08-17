5e25c3836d1e5ee9f7b8e07d44744228bc7b2710
Mahmoud Khawaja <mahmoud.khawaja97@gmail.com>
2025-08-18 02:43:36 +0300

    rollback

--- src/classes/modules/java.base/java/net/Socket.java
+++ src/classes/modules/java.base/java/net/Socket.java
@@ -4,28 +4,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-/** 
+/**
  * Model class for java.net.Socket
- * 
+ *
  * @author Nastaran Shafiei
  */
 public class Socket implements java.io.Closeable {
 
+  static {
+    System.out.println("DEBUG: JPF Socket MODEL CLASS loaded successfully!");
+    try {
+      System.out.println("DEBUG: Socket model class location: " +
+              Socket.class.getProtectionDomain().getCodeSource().getLocation());
+    } catch (UnsupportedOperationException e) {
+      System.out.println("DEBUG: Socket model class location: [JPF environment - getProtectionDomain not supported]");
+    }
+  }
+
   private SocketInputStream input = null;
   private SocketOutputStream output = null;
-  
+
   /**
-   *  This is used to make JPF account for connection objects when state matching. This 
-   *  keep the hash value of this socket connection object, and it is updated upon any 
-   *  internal change to the connection
+   * This is used to make JPF account for connection objects when state matching.
+   * Keep the hash value of this socket connection object, and it is updated upon any
+   * internal change to the connection
    */
   private int hash;
-  
+
   /**
    * The implementation of this Socket.
    */
-  SocketImpl impl; 
-  
+  SocketImpl impl;
+
   /**
    * Various states of this socket.
    */
@@ -38,13 +48,16 @@ public class Socket implements java.io.Closeable {
   private Thread waitingThread;
 
   public Socket() {
+    System.out.println("DEBUG: Socket MODEL CLASS default constructor called");
     impl = new SocketImpl();
     impl.setSocket(this);
     impl.bind(-1);
     this.setIOStream();
+    System.out.println("DEBUG: Socket() constructor completed, hash=" + hash);
   }
 
   public Socket(String host, int port) throws UnknownHostException, IOException {
+    System.out.println("DEBUG: Socket MODEL CLASS constructor called with host=" + host + ", port=" + port);
     SocketImpl.checkPort(port);
     impl = new SocketImpl();
     impl.setSocket(this);
@@ -53,87 +66,95 @@ public class Socket implements java.io.Closeable {
     impl.port = port;
     this.setIOStream();
     connect(host, port);
+    System.out.println("DEBUG: Socket(host, port) constructor completed, connected=" + connected);
   }
 
   private void setIOStream() {
+    System.out.println("DEBUG: Socket MODEL CLASS setIOStream() called");
     this.input = new SocketInputStream(this);
     this.output = new SocketOutputStream(this);
   }
 
-  public Socket(InetAddress address, int port) throws UnknownHostException, IOException {
-    this(address.getHostName(), port);
-  }
-
   private native void connect(String host, int port) throws IOException;
+
   public int getHash() {
+    System.out.println("DEBUG: Socket MODEL CLASS getHash() called, returning hash=" + hash);
     return this.hash;
   }
+
   public void connect(SocketAddress endpoint) throws IOException {
+    System.out.println("DEBUG: Socket MODEL CLASS connect(SocketAddress) called");
     connect(endpoint, 0);
   }
-  
+
   public void connect(SocketAddress endpoint, int timeout) throws IOException {
-    // TODO: for timeout, look at wait() implementation
+    System.out.println("DEBUG: Socket MODEL CLASS connect(SocketAddress, timeout=" + timeout + ") called");
     String host = ((InetSocketAddress)endpoint).getHostName();
     int port = ((InetSocketAddress)endpoint).getPort();
     connect(host, port);
   }
 
   void setBound() {
+    System.out.println("DEBUG: Socket MODEL CLASS setBound() called");
     bound = true;
   }
 
   public InputStream getInputStream() throws IOException {
+    System.out.println("DEBUG: Socket MODEL CLASS getInputStream() called, closed=" + closed + ", connected=" + connected);
     if (isClosed()) {
       throw new SocketException("Socket is closed");
     } else if (!isConnected()) {
       throw new SocketException("Socket is not connected");
     }
-    
+
     assert(!isClosed());
     assert(isConnected());
-    
+
     return input;
   }
 
   public OutputStream getOutputStream() throws IOException {
+    System.out.println("DEBUG: Socket MODEL CLASS getOutputStream() called, closed=" + closed + ", connected=" + connected);
     if (isClosed()) {
       throw new SocketException("Socket is closed");
     } else if (!isConnected()) {
       throw new SocketException("Socket is not connected");
     }
-    
+
     assert(!isClosed());
     assert(isConnected());
-    
+
     return output;
   }
-  
+
   /**
    * Returns the closed state of the socket.
    */
   public boolean isClosed() {
     return this.closed;
   }
-  
+
   /**
    * Returns the connection state of the socket.
    */
   public native boolean isConnected();
-  
-  // TODO: Any thread currently blocked in an I/O operation upon this socket 
-  //       will throw a SocketException.
-  // TODO: Throws IOException, if an I/O error occurs when closing the socket
+
   @Override
   public native void close() throws IOException;
-  
+
   private int timeout;
   public void setSoTimeout(int timeout) {
+    System.out.println("DEBUG: Socket MODEL CLASS setSoTimeout(" + timeout + ") called");
     this.timeout = timeout;
   }
-  
+
   @Override
   protected void finalize() throws Throwable{
+    System.out.println("DEBUG: Socket MODEL CLASS finalize() called");
     close();
+  }
+
+  public Socket(InetAddress address, int port) throws UnknownHostException, IOException {
+    this(address.getHostName(), port);
   }
 }
--- src/tests/net/nas/SocketTest.java
+++ src/tests/net/nas/SocketTest.java
@@ -1,13 +1,11 @@
 package net.nas;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
+import java.net.*;
 
 import nas.util.test.TestNasJPF;
 
@@ -18,95 +16,55 @@ import gov.nasa.jpf.util.test.TestMultiProcessJPF;
 
 public class SocketTest extends TestNasJPF {
   String[] args = { "+search.multiple_errors = true",
-                    "+vm.process_finalizers = true",
-                    "+vm.nas.initiating_target = 0",
-                    "+vm.serializer.class=",
-                    "+vm.storage.class="
+          "+vm.process_finalizers = true",
+          "+vm.nas.initiating_target = 0"
   };
-  
+
   int port = 1024;
   final String HOST = "localhost";
-  
+
   @Test
   public void testEstablishingConnection() throws IOException {
     if (mpVerifyNoPropertyViolation(2, args)) {
-      
+
       switch(getProcessId()) {
-      case 0:
-        ServerSocket serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(10);
-        Socket sock1 = null;
-        try {
-          sock1 = serverSocket.accept();
-        } catch(SocketTimeoutException e) {
-          // gets here if no request is coming after a certain amount of time
-          assertNull(sock1);
-        }
-        break;
-        
-      case 1:
-        Socket sock2;
-        try {
-          sock2 = new Socket(HOST, port);
-          assertTrue(sock2.isConnected());
-        } catch(IOException e) {
-          // gets here if there was no server accepting the connection request
-        }
-        break;
+        case 0:
+          ServerSocket serverSocket = new ServerSocket(port);
+          serverSocket.setSoTimeout(10);
+          Socket sock1 = null;
+          try {
+            sock1 = serverSocket.accept();
+          } catch(SocketTimeoutException e) {
+            // gets here if no request is coming after a certain amount of time
+            assertNull(sock1);
+          }
+          break;
+
+        case 1:
+          Socket sock2;
+          try {
+            sock2 = new Socket(HOST, port);
+            assertTrue(sock2.isConnected());
+          } catch(IOException e) {
+            // gets here if there was no server accepting the connection request
+          }
+          break;
       }
     }
   }
-  
+
   /**
    * Blocking accept with timeout throws SocketTimeoutException
    */
   @Test
   public void testTimedoutAccept() throws IOException {
     if (mpVerifyUnhandledException(1, "java.net.SocketTimeoutException", args)) {
+
       ServerSocket serverSocket = new ServerSocket(port);
       serverSocket.setSoTimeout(10);
-
-      // Check if we're in minimal mode
-      boolean isMinimalMode = isMinimalMode(serverSocket);
-
-      if (isMinimalMode) {
-        // In minimal mode, timeout mechanisms may not work properly
-        // So we simulate the expected timeout exception
-        try {
-          serverSocket.accept();
-          // If accept() doesn't timeout naturally in minimal mode,
-          // throw the expected exception to satisfy the test framework
-          throw new SocketTimeoutException("Accept timed out (simulated for minimal mode compatibility)");
-        } catch (SocketTimeoutException expected) {
-          // Re-throw the timeout exception for mpVerifyUnhandledException
-          throw expected;
-        }
-      } else {
-        // Normal mode - expect standard timeout behavior
-        serverSocket.accept(); // This should timeout and throw SocketTimeoutException
-      }
+      serverSocket.accept();
     }
   }
-
-
-  // Helper method to detect minimal mode
-  private boolean isMinimalMode(Object obj) {
-    try {
-      // Try to access a field that should exist in normal mode
-      Field field = obj.getClass().getDeclaredField("impl");
-      field.setAccessible(true);
-      Object impl = field.get(obj);
-      if (impl != null) {
-        // Check if impl has proper field structure
-        Field boundField = impl.getClass().getDeclaredField("localPort");
-        return false; // Normal mode
-      }
-      return true; // Minimal mode
-    } catch (Exception e) {
-      return true; // Assume minimal mode if we can't access fields
-    }
-  }
-
 
   private int getHash(Socket sock) throws Exception {
     try {
@@ -124,87 +82,78 @@ public class SocketTest extends TestNasJPF {
   @Test
   public void testHash() throws Exception {
     if (mpVerifyNoPropertyViolation(2, args)) {
-      switch (getProcessId()) {
+
+      switch(getProcessId()) {
         case 0:
           ServerSocket serverSocket = new ServerSocket(port);
           Socket sock1 = serverSocket.accept();
 
           int h1 = getHash(sock1);
-          assertTrue("Initial hash must be non-negative", h1 >= 0);
+          assertTrue(h1!=0);
 
           OutputStream socketOutput = sock1.getOutputStream();
+
           try {
             socketOutput.write(10);
-            Thread.sleep(1); // allow peer to update fallback hash
             int h2 = getHash(sock1);
-
-            // Check if the hash field actually exists via reflection
-            try {
-              Field hashField = sock1.getClass().getDeclaredField("hash");
-              hashField.setAccessible(true);
-              // Field exists - require it to change
-              assertTrue("Hash should change after write: h1=" + h1 + ", h2=" + h2,
-                      h1 != h2);
-            } catch (NoSuchFieldException e) {
-              // Minimal mode - hash field doesn't exist, just verify non-negativity
-              System.out.println("Running in minimal mode - hash field doesn't exist");
-              assertTrue("Fallback hash must be non-negative: h2=" + h2, h2 >= 0);
-            }
-          } catch (SocketException ignored) {
-            // acceptable under JPF-NAS
+            assertTrue(h1!=h2);
+          } catch(SocketException e) {
+            // attempting to write on a close connection
           }
-          break;
 
+          break;
         case 1:
+          Socket sock2;
           try {
-            Socket sock2 = new Socket(HOST, port);
-            assertTrue(sock2.isConnected());
-          } catch (IOException ignored) {
+            sock2 = new Socket(HOST, port);
+
+
+          } catch(IOException e) {
+            // gets here if there was no server accepting the connection request
+            System.out.println("never stablished!!");
           }
           break;
       }
     }
   }
 
-
-  
   @Test
   public void testTimedoutRead() throws IOException {
     if (mpVerifyNoPropertyViolation(2, args)) {
-      
+
       switch(getProcessId()) {
-      case 0:
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket sock1 = serverSocket.accept();
-        assertTrue(sock1.isConnected());
-        break;
-        
-      case 1:
-        Socket sock2 = null;
-        try {
-          sock2 = new Socket(HOST, port);
-          sock2.setSoTimeout(10);
-          assertTrue(sock2.isConnected());
-        } catch(IOException e) {
-          // gets here if there was no server accepting the connection request
-          return;
-        }
-        
-        try {
-          InputStream in = sock2.getInputStream();
-          in.read();
-          //assertTrue(isOtherEndClosed());
-        } catch(SocketTimeoutException e) {
-          return;
-        } catch(SocketException e) {
-          return;
-        }
-        
-        break;
+        case 0:
+          ServerSocket serverSocket = new ServerSocket(port);
+          Socket sock1 = serverSocket.accept();
+          assertTrue(sock1.isConnected());
+          break;
+
+        case 1:
+          Socket sock2 = null;
+          try {
+            sock2 = new Socket(HOST, port);
+            sock2.setSoTimeout(10);
+            assertTrue(sock2.isConnected());
+          } catch(IOException e) {
+            // gets here if there was no server accepting the connection request
+            return;
+          }
+
+          try {
+            InputStream in = sock2.getInputStream();
+            in.read();
+            //assertTrue(isOtherEndClosed());
+          } catch(SocketTimeoutException e) {
+            return;
+          } catch(SocketException e) {
+            return;
+          }
+
+          break;
       }
     }
   }
-  
+
   static class FinalizeServer {
     @Override
     protected void finalize() throws Throwable {
@@ -213,42 +162,30 @@ public class SocketTest extends TestNasJPF {
       System.out.println("DONE!");
     }
   }
-  
-  // This is testing finalizer threads. It should belong to jpf-core, but I add it here 
+
+  // This is testing finalizer threads. It should belong to jpf-core, but I add it here
   // since it needs support for ServerSocket.accept().
   // here we make sure that MultiProcessVM does not ignore deadlocks in finalizers.
   @Test
   public void testDeadlockForBlockedFinalizer_MulitProcessVM() {
-    // Check if minimal mode is active
-    boolean isMinimalMode = true; // Assume minimal mode for Java 11
-
-    if (isMinimalMode) {
-      // In minimal mode, deadlock detection in finalizers is limited
-      // Test basic finalizer execution without System.runFinalization()
-      if (mpVerifyNoPropertyViolation(1, "+vm.process_finalizers=true")) {
-        System.out.println("Running finalizer test in minimal mode");
-        FinalizeServer server = new FinalizeServer();
-        server = null; // Make eligible for finalization
-
-        // Force garbage collection to trigger finalizer
-        // Remove the problematic System.runFinalization() call
-        System.gc();
-
-        // In minimal mode, we just verify that the test runs without crashing
-        // The finalizer will be triggered by JPF's garbage collection mechanism
-        try {
-          Thread.sleep(100); // Give finalizer time to run
-        } catch (InterruptedException ignored) {}
-
-        // Test passes if we reach here without exceptions
-      }
-    } else {
-      // Normal mode - run original deadlock detection
-      if (mpVerifyDeadlock(1, "+vm.process_finalizers=true")) {
-        new FinalizeServer();
-      }
+    if (mpVerifyDeadlock(1, "+vm.process_finalizers=true")){
+      new FinalizeServer();
     }
   }
 
+  @Test
+  public void debugModelClassLoading() {
+    try {
+      Socket socket = new Socket();
+      Field hashField = socket.getClass().getDeclaredField("hash");
+      System.out.println("SUCCESS: Hash field exists in Socket model class!");
+
+      ServerSocket serverSocket = new ServerSocket();
+      System.out.println("SUCCESS: ServerSocket model class instantiated!");
+
+    } catch (Exception e) {
+      System.out.println("Model classes not loading properly: " + e.getMessage());
+    }
+  }
 
 }
